a60329f989e7b6fc6e68ffa53be8e669198cd8ce
brad213 <braddebaile@gmail.com>
2019-12-01 22:30:36 +0900

    어노테이션으로 게시판 만들기 @Controller,@RequestMapping CRUD통합컨트롤러 BoardController 만들기

--- BoardWeb/src/main/java/com/springbook/biz/user/impl/UserDAO.java
+++ BoardWeb/src/main/java/com/springbook/biz/user/impl/UserDAO.java
@@ -25,16 +25,16 @@ public class UserDAO {
 	//회원등록
 
 	public UserVO getUser(UserVO vo) {
-		UserVO user=null;
+		UserVO user= null;
 		try {
 			System.out.println("===> JDBC로 getUser() 기능 처리");
-			conn=JDBCUtil.getConnection();
-			stmt=conn.prepareStatement(USER_GET);
+			conn= JDBCUtil.getConnection();
+			stmt= conn.prepareStatement(USER_GET);
 			stmt.setString(1, vo.getId());
 			stmt.setString(2, vo.getPassword());
-			rs=stmt.executeQuery();
-			if(rs.next()) {
-				user=new UserVO();
+			rs= stmt.executeQuery();
+			if (rs.next()) {
+				user = new UserVO();
 				user.setId(rs.getString("ID"));
 				user.setPassword(rs.getString("PASSWORD"));
 				user.setName(rs.getString("NAME"));
